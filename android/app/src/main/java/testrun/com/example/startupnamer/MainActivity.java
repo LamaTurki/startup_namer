22e0a1c325370e381f27b375cdf62ea9ec343f71
Lama <lama@Lamas-MacBook-Pro.local>
2019-01-16 20:53:43 +0300

    Initial commit

---
+++ android/app/src/main/java/testrun/com/example/startupnamer/MainActivity.java
@@ -0,0 +1,13 @@
+package testrun.com.example.startupnamer;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
